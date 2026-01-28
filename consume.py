3c39d58e08729061e2a97a74cd8d72da1cea0fee
luis-octavius <luisoctavius.sc@gmail.com>
2026-01-28 20:40:00 -0300

    feat: add methods to reconstruct files url and download files based on the generated urls

--- consume.py
+++ consume.py
@@ -1,3 +1,4 @@
+import os
 import re
 
 import requests
@@ -17,6 +18,38 @@ def get_html(url):
         print("Error: ", e)
 
 
+def download_files(files, file_names):
+    for i in range(0, len(files), 1):
+        download = requests.get(files[i])
+        file_path = os.path.join("./download", file_names[i])
+        print("File path: ", file_path)
+
+        if download.status_code == 200:
+            with open(file_path, "wb") as file:
+                file.write(download.content)
+            print("File downloaded successfully")
+        else:
+            print("Failed to download file")
+
+
+def get_files_url(html, url):
+    soup = BeautifulSoup(html, "html.parser")
+
+    table_rows = soup.find_all("a")
+
+    files = []
+    file_names = []
+
+    for row in table_rows:
+        str = row.string
+        if re.search("(.zip)", str):
+            file = os.path.join(url, str)
+            files.append(file)
+            file_names.append(str)
+
+    return files, file_names
+
+
 def get_last_year_url(html, url):
     soup = BeautifulSoup(html, "html.parser")
 
@@ -34,7 +67,7 @@ def extract_last_year(table_rows):
 
     for row in table_rows:
         str = row.string.replace("/", "")
-        if re.search("\d", str):
+        if re.search(r"\d", str):
             extracted_years.append(str)
 
     return extracted_years[-1:][0]
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from consume import get_html, get_last_year_url
+from consume import download_files, get_files_url, get_html, get_last_year_url
 
 
 def main():
     url = "https://dadosabertos.ans.gov.br/FTP/PDA/demonstracoes_contabeis/"
+
     page = get_html(url)
+
     build_url = get_last_year_url(page, url)
-    print(build_url)
+
+    years_page_html = get_html(build_url)
+
+    files_url, files_name = get_files_url(years_page_html, build_url)
+
+    download_files(files_url, files_name)
 
 
 if __name__ == "__main__":
